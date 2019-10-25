532a736edeb2118f6433c02fe27b8b33b12d0b3d
Hubert <hubert.lindenau@gmail.com>
2019-10-25 20:53:45 +0200

    Add files via upload

--- cpp/PCmax/PCmax.cpp
+++ cpp/PCmax/PCmax.cpp
@@ -5,7 +5,15 @@
 #include <iostream>
 #include <stdio.h> 
 #include <algorithm>
+#include <chrono>
 
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		std::cout << arr[i] << " ";
+	std::cout << std::endl;
+}
 
 int index_najmniejszego_elementu(int arr[], int size)
 {
@@ -35,13 +43,16 @@ int main()
 	zadania = new int[liczba_zadan];
 	procesory = new int[liczba_procesorow] {0};
 
-		
+		// Wpisywanie czasow zadan z wejscia do tablicy
 		for (int n = 0; n < liczba_zadan; n++)
 		{
 			std::cin >> zadania[n];
 		}	
-
 		std::cout << "\nLiczba procesorow: " << liczba_procesorow << " liczba zadan: " << liczba_zadan << std::endl;
+
+		//auto start = std::chrono::high_resolution_clock::now();
+		
+		std::cout << "Algorytm listowy: \n";
 
 		for (int i = 0; i < liczba_zadan; i++)
 		{
@@ -52,13 +63,53 @@ int main()
 				break;
 			}
 		}
+
+		/* 
+				std::cout << "Czasy wykonania zadan przez poszczegolne procesory:\n";
+		for (int i = 0; i < liczba_procesorow; i++)
+		{
+			std::cout << i + 1 << ". " << procesory[i] << ", ";
+		}
+		*/
+
+		// auto stop = std::chrono::high_resolution_clock::now();
+		std::cout << "Cmax = " << *std::max_element(procesory, procesory + liczba_procesorow) << std::endl;
+
+		//std::chrono::duration<double> elapsed = stop - start;
+		//std::cout << elapsed.count() << " s\n" << std::endl;
+
+		std::cout << "Algorytm zach³anny:\n";
+
+		//auto start2 = std::chrono::high_resolution_clock::now();
+
+		std::sort(zadania, zadania + liczba_zadan, std::greater<int>());
+		for (int i = 0; i < liczba_procesorow; i++)
+		{
+			procesory[i] = 0;
+		}
+		for (int i = 0; i < liczba_zadan; i++)
+		{
+			for (int j = i % liczba_procesorow; j < liczba_procesorow; j++)
+			{
+				if (procesory[j] == 0) procesory[j] += zadania[i];
+				else procesory[index_najmniejszego_elementu(procesory, liczba_procesorow)] += zadania[i];
+				break;
+			}
+		}
+
+		/*
 		std::cout << "Czasy wykonania zadan przez procesory:\n";
 		for (int i = 0; i < liczba_procesorow; i++)
 		{
-			std::cout << i + 1 << ". " << procesory[i] << std::endl;
+			std::cout << i + 1 << ". " << procesory[i] << ", ";
 		}
+		*/
+		//auto stop2 = std::chrono::high_resolution_clock::now();
 
-		std::cout << "Cmax = " << *std::max_element(procesory, procesory + liczba_procesorow) << std::endl;
+		std::cout << "\nCmax = " << *std::max_element(procesory, procesory + liczba_procesorow) << std::endl;
+		
+		//std::chrono::duration<double> elapsed2 = stop2 - start2;
+		//std::cout << elapsed.count() << " s\n" << std::endl;
 
 		std::getchar();
 		delete[] zadania;
